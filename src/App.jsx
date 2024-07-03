cc610b076e4eb6fac9104388e82b022574ee929f
gabeyong4 <gabe.yong4@gmail.com>
2024-07-03 16:00:14 +0800

    finished card components section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Card from './Card.jsx'
+import './App.css'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </>
   );
 }
