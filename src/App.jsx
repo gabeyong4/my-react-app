efa284a097be9204b3ede2e22999f4ea1d746f15
gabeyong4 <gabe.yong4@gmail.com>
2024-07-08 23:34:07 +0800

    Rendering a List section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
 import './App.css'
 
 function App() {
+  const fruits = [
+    {id:1, name: "apple", calories: 95},
+    {id:2, name: "orange", calories: 45},
+    {id:3, name: "banana", calories: 105},
+    {id:4, name: "coconut", calories: 159},
+    {id:5, name: "pineapple", calories: 37},
+  ];
+
+  const family = [
+    {id:6, name: "quinn", calories: 60},
+    {id:7, name: "gabe", calories: 100},
+    {id:8, name: "toto", calories: 8 },
+    {id:9, name: "coconut", calories: 20},
+    {id:10, name: "freddy", calories: 1},
+  ];
+
   return (
     <>
-    <UserGreeting isLoggedIn={false} username="totomoney"> </UserGreeting>
+    {fruits.length > 0 && <List items={fruits} category="Fruits"></List>}
+
+    {family.length > 0 && <List items={family} category="Family"></List>}
     </>
   );
 }
