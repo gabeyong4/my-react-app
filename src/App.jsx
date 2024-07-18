5de4968b4374d19a6014a60d24fd856828d49691
gabeyong4 <gabe.yong4@gmail.com>
2024-07-18 23:38:11 +0800

    color picker app section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import MyComponent from './MyComponent.jsx'
+import ColorPicker from './ColorPicker.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
-    <MyComponent/>
+    <ColorPicker/>
     </>
   );
 }
